b4b440a9256354a4d8b9cb44f995a813edb9ecee
Badillo Victor <victtorbadillo@hotmail.com>
2023-12-03 12:33:46 -0600

    Primer Problema

--- ProgramacionDeterminista.py
+++ ProgramacionDeterminista.py
@@ -5,7 +5,7 @@ def calcular_distancia(origen, destino, futuro):
 def obtener_destinos(origen):
     destinos = []
     for i in range(len(grafo[origen])):
-        if not grafo[origen][i] == 100:
+        if not grafo[origen][i] == -1:
             destinos.append(i)
     return destinos
 
@@ -13,7 +13,7 @@ def obtener_destinos(origen):
 def obtener_origenes(destino):
     origenes = []
     for i in range(len(grafo)):
-        if not grafo[i][destino] == 100:
+        if not grafo[i][destino] == -1:
             origenes.append(i)
     return origenes
 
@@ -35,8 +35,8 @@ def print_tabla(origenes, n, optimos):
             solucion = [diccionario[i]]
             for j in range(len(origenes)):
                 solucion.append(str(calcular_distancia(i, origenes[j], int(optimos[j]))))
-            solucion.append(str(min(solucion[1:])))
-            nuevos_optimos.append(min(solucion[1:]))
+            solucion.append(str(max(solucion[1:])))
+            nuevos_optimos.append(max(solucion[1:]))
             soluciones.append(solucion)
         for s in soluciones:
             print('\t'.join(s))
@@ -45,30 +45,38 @@ def print_tabla(origenes, n, optimos):
 
 
 grafo = [
-    [100, 4, 3, 100, 100, 100, 100, 100, 100, 100],
-    [100, 100, 100, 3, 3, 4, 100, 100, 100, 100],
-    [100, 100, 100, 3, 6, 1, 100, 100, 100, 100],
-    [100, 100, 100, 100, 100, 100, 5, 4, 6, 100],
-    [100, 100, 100, 100, 100, 100, 1, 2, 4, 100],
-    [100, 100, 100, 100, 100, 100, 4, 3, 7, 100],
-    [100, 100, 100, 100, 100, 100, 100, 100, 100, 3],
-    [100, 100, 100, 100, 100, 100, 100, 100, 100, 4],
-    [100, 100, 100, 100, 100, 100, 100, 100, 100, 2],
-    [100, 100, 100, 100, 100, 100, 100, 100, 100, 100]
+    [-1, 130, 100, 80, 70, 50, 0, -1, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, 20, 0, -1, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, 45, 20, 0, -1, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, 75, 45, 20, 0, -1, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, 110, 75, 45, 20, 0, -1, -1],
+    [-1, -1, -1, -1, -1, -1, -1, 150, 110, 75, 45, 20, 0, -1],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 45],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 70],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 90],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 105],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 120],
+    [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
 ]
 
 diccionario = {
-    0: 'J',
-    1: 'I',
-    2: 'H',
-    3: 'G',
-    4: 'F',
-    5: 'E',
-    6: 'D',
-    7: 'C',
-    8: 'B',
-    9: 'A',
+    0: 'N',
+    1: 'M',
+    2: 'L',
+    3: 'K',
+    4: 'J',
+    5: 'I',
+    6: 'H',
+    7: 'G',
+    8: 'F',
+    9: 'E',
+    10: 'D',
+    11: 'C',
+    12: 'B',
+    13: 'A',
 }
 
 if __name__ == '__main__':
-    print_tabla([0], 4, [0])
+    print_tabla([0], 3, [0])
