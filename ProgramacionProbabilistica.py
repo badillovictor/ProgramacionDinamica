8618ad58ec5415a4d924dee7abe86b7d94bea977
Badillo Victor <victtorbadillo@hotmail.com>
2023-12-03 14:48:38 -0600

    Primer Problema

--- ProgramacionProbabilistica.py
+++ ProgramacionProbabilistica.py
@@ -1,20 +1,71 @@
-def calcular_probA(inicial):
-    if inicial - 5000 < 0:
+def calcular_probA_inicial(actual):
+    if actual == 0:
         return 0
     else:
-        return 0.3 * (inicial - 5000) + 0.7 * (inicial + 5000)
+        return 0.3 * (actual - 5000) + 0.7 * (actual + 5000)
 
 
-def calcular_probB(inicial):
-    return 0.9 * inicial + 0.1 * (inicial + 5000)
+def calcular_probB_inicial(actual):
+    if actual == 0:
+        return 0
+    else:
+        return 0.9 * actual + 0.1 * (actual + 5000)
 
 
-def creacion_grafo(grafo: set, limite: int):
-    if limite != 0:
-        for e in grafo:
-            if e != 0:
-                grafo.update([e-5000, e, ])
+def calcular_probA(actual, x, y):
+    if actual == 0:
+        return 0
+    else:
+        return 0.3 * x + 0.7 * y
+
+
+def calcular_probB(actual, x, y):
+    if actual == 0:
+        return 0
+    else:
+        return 0.9 * x + 0.1 * (actual + y)
+
+
+def print_tabla_inicial(n, diccionario):
+    encabezado = ('\t'*2).join(['s{0}'.format(n),
+                            ('\t'*2).join(['A', 'B']),
+                            'f*(s{0})'.format(n)
+                            # ,'x*({0})'.format(n),
+                            ])
+    print(encabezado)
+    nuevodict = {}
+    for e in diccionario:
+        solucion = [str(diccionario[e])]
+        solucion.append(str(calcular_probA_inicial(diccionario[e])))
+        solucion.append(str(calcular_probB_inicial(diccionario[e])))
+        temp = solucion.append(str(max(solucion[1:])))
+        nuevodict.update(diccionario[e], temp)
+        print('\t'.join(solucion))
+    print('\n')
+    print_tabla_posterior(n-1, nuevodict)
+
+
+def print_tabla_posterior(n, diccionario):
+    if not n == 0:
+        encabezado = ('\t'*2).join(['s{0}'.format(n),
+                                ('\t'*2).join(['A', 'B']),
+                                'f*(s{0})'.format(n)
+                                # ,'x*({0})'.format(n),
+                                ])
+        print(encabezado)
+        for e in diccionario:
+            solucion = [str(diccionario[e])]
+            solucion.append(str(calcular_probA_inicial(diccionario[e])))
+            solucion.append(str(calcular_probB_inicial(diccionario[e])))
+            solucion.append(str(max(solucion[1:])))
+            print('\t'.join(solucion))
+        print('\n')
 
 
 if __name__ == '__main__':
-    pass
+    print_tabla_inicial(3, {
+        0: 0,
+        5000: 5000,
+        10000: 10000,
+        15000: 15000,
+    })
